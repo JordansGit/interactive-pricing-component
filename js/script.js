59af3f2eebda2c4f4db4d5766cc53a1c51c8ee17
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2022-05-26 16:30:27 +0100

    added slider output to 2dp

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ var slider = document.getElementById("slider");
 var output = document.getElementById("cost");
 output.innerHTML = slider.value; // Display the default slider value
 
+/* make slider value display 16.00 by default */ 
+window.onload = function() {
+  slider.value = "16";
+  cost.innerText = "16.00";
+}
+
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  // if this.value is not to 2 decimal places, this.value = this.value + ".00" 
-  output.innerHTML = this.value;
+  var x = Number(this.value).toFixed(2);
+  output.innerHTML = x;
 }
 
 
+/* learning notes: 
+oninput = gets the slider input, also each time the slider moves, the input value changes.  
+this.value gets the value of oninput at its current location. 
+
+
+*/
